316a1f2739ed6e9571138ac2499720371eedec73
Jeovana Miranda <jeovanamiranda96@gmail.com>
2025-12-08 12:37:03 -0300

    teste2

--- import.js
+++ import.js
@@ -1,9 +1,9 @@
 // Carrega o header
-fetch('/ConfeitariaDoceEncanto/components/nav/nav.html')
+fetch('/components/nav/nav.html')
   .then(res => res.text())
   .then(data => document.getElementById('nav').innerHTML = data);
 
 // Carrega o footer
-fetch('/ConfeitariaDoceEncanto/components/footer/footer.html')
+fetch('/components/footer/footer.html')
   .then(res => res.text())
   .then(data => document.getElementById('footer').innerHTML = data);
